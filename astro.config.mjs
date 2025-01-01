42f50e639416b1846a53699f451cd682c374369d
azerxafro <ashwinazer@gmail.com>
2025-01-01 21:33:16 +0530

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',  // SSR or Hybrid rendering
+  adapter: netlify(), // Add the Netlify adapter for SSR
 });
